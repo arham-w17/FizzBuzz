9712d53a7e8ba50207595159ec4b9524fed3a371
Arham <arhamwajid2005@gmail.com>
2024-09-12 02:18:07 -0400

    commit 3 (before using git pull)

---
+++ src/Multiples.java
@@ -0,0 +1,19 @@
+public class Multiples
+{
+    public static void main(String[] args)
+    {
+        int ctr = 0;
+        for(int i = 1; i <= 1000; i++)
+        {
+            if (i % 3 == 0)
+            {
+                ctr++;
+            }
+            else if (i % 5 == 0)
+            {
+                ctr++;
+            }
+        }
+        System.out.println(ctr);
+    }
+}
